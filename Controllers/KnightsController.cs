586d5dc854f5bb19f8e47921c30a3e5800a46b4e
ChesterJGreen <seejarom@gmail.com>
2021-09-01 14:24:44 -0600

    startup working

--- Controllers/KnightsController.cs
+++ Controllers/KnightsController.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using kingdom.Models;
+using kingdom.Services;
+using Microsoft.AspNetCore.Mvc;
 namespace kingdom.Controllers
 {
     [ApiController]
